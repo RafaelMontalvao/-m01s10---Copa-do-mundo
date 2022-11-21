8459a578a98b7ecda39b8307ab7a3b2453591c46
RafaelMontalvao <rushbig@hotmail.com>
2022-11-20 22:04:10 -0300

    finalizando exerciocio semana 10

---
+++ src/test/java/tech/devinhouse/copadomundo/M01sApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.copadomundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M01sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
